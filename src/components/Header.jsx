f9db23621beb4939f2a80810449e958e7d0fdca6
hmgny <hmerveguney@hotmail.com>
2024-12-05 23:45:06 +0300

    Hi! img güncellendi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -64,7 +64,7 @@ function Header() {
               <p className="text-2xl lg:text-4xl xl:text-5xl">{content.hi} </p>
               <img
                 className="w-8 lg:w-16"
-                src="/images/Hi!.png"
+                src="/images/hi!.png"
                 alt="el simgesi"
               />
             </div>
